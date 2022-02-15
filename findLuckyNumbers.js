7fbc569daf603630749a9de7cc86e2f4c3f08c73
Isarel Getu <israelgetu101@gmail.com>
2022-02-15 14:49:56 -0600

    Solved

--- findLuckyNumbers.js
+++ findLuckyNumbers.js
@@ -1,1 +1,12 @@
 // Write your code below this line.
+function luckyNumbers(n) {
+  let result = new Set();
+  while (result.size < n) {
+    let randomNum = Math.floor(Math.random() * n);
+    result.add(randomNum);
+  }
+  //console.log(result)
+  return result;
+}
+
+console.log(luckyNumbers(5));
